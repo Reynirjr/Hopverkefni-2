a085f98d850aa75c0ea74dba1d0b7eff67e1dc9a
Reynirjr <brj46@hi.is>
2023-11-21 22:30:54 +0000

    lagaði linkinn á vorusida.html

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=6')
     data.items.forEach(product => {
         const markup = `
             <li>
+                <a href="./sidur/vorusida.html?id=${product.id}">
                 <img src="${product.image}" alt="${product.title}" style="width: 250px; height: 150px; object-scale-down;">
                 <div>
                     <h3>${product.title}</h3>
