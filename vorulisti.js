4754f65bf12aee5b7c7f43c53e87f3b50be6c7d9
Reynirjr <brj46@hi.is>
2023-11-22 20:24:06 +0000

    linkur að vörusíðu úr vörulista

--- vorulisti.js
+++ vorulisti.js
@@ -5,6 +5,7 @@ fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products')
     .then(data => {
             data.items.forEach(product => {
                 const productMarkup = `
+                <a href="./vorusida.html?id=${product.id}">
                 <h2>${product.title}</h2>
                 <img src="${product.image}" alt="${product.title}">
                 <p>Flokkur: ${product.category_title}</p>
